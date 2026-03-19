1a7c19615335960b86c1883be45bf799950234eb
AnubhavShukla09 <56950352+AnubhavShukla09@users.noreply.github.com>
2026-03-19 20:56:49 +0530

    Update Find First and Last Position of Element in Sorted Array.cpp

--- Find First and Last Position of Element in Sorted Array.cpp
+++ Find First and Last Position of Element in Sorted Array.cpp
@@ -7,3 +7,60 @@ public:
         return {-1, -1};
     }
 };
+
+class Solution {
+public:
+
+    // Find first occurrence of target
+    int findFirst(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int ans = -1;                                      // store result
+        
+        while(l <= r) {
+            int mid = l + (r - l) / 2;                     // avoid overflow
+            
+            if(nums[mid] == target) {
+                ans = mid;                                // potential answer
+                r = mid - 1;                              // move left to find earlier occurrence
+            }
+            else if(nums[mid] < target) {
+                l = mid + 1;                              // go right
+            }
+            else {
+                r = mid - 1;                              // go left
+            }
+        }
+        
+        return ans;                                       // first index or -1
+    }
+
+    // Find last occurrence of target
+    int findLast(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int ans = -1;                                      // store result
+        
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            
+            if(nums[mid] == target) {
+                ans = mid;                                // potential answer
+                l = mid + 1;                              // move right to find later occurrence
+            }
+            else if(nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        
+        return ans;                                       // last index or -1
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = findFirst(nums, target);               // left boundary
+        int last = findLast(nums, target);                 // right boundary
+        
+        return {first, last};                              // return result
+    }
+};
